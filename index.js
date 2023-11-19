5d257d9ec49de6d293999e4d3190c33be97b9457
gurudas <angadiparambilgurudas@gmail.com>
2023-11-19 19:03:31 +0530

    added edit and delete options

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser'; 
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 // Get the current file URL
 const __filename = fileURLToPath(import.meta.url);
@@ -10,8 +11,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const posts = [
-  { id: '1', title: 'First Post', content: 'This is the content of the first post.' },
-  { id: '2', title: 'Second Post', content: 'This is the content of the second post.' },
   // Add more posts as needed
 ];
 
@@ -36,7 +35,7 @@ app.use((req, res, next) => {
   
     // Call next() to pass control to the next middleware in the stack
     next();
-  });
+});
   
 
 // Home route
@@ -53,14 +52,17 @@ app.get('/create', (req, res) => {
 app.post('/create', (req, res) => {
   const { title, content } = req.body;
 
+  const generatedUuid = uuidv4();
+  const integerUuid = BigInt(`0x${generatedUuid.replace(/-/g, '')}`);
+
   // Assuming posts have unique IDs (e.g., generated with UUID)
-  const newPost = { id: Math.random().toString(), title, content };
+  const newPost = { id: integerUuid.toString(), title, content };
 
   // Add the new post to the array (or save it to a database)
   posts.push(newPost);
 
   // Redirect to the home page after creating the post
-  res.redirect('/');
+  res.redirect(303, '/');
 });
 
 // View posts route
@@ -68,10 +70,58 @@ app.get('/view', (req, res) => {
     res.render('post_list', { posts });
 });
 
+// update requeast
+app.post('/update/:id', (req, res) => {
+  const postId = parseInt(req.params.id, 10);
+  const { title, content } = req.body;
+  
+  // Find the index of the post with the specified id
+  const postIndex = posts.findIndex(post => post.id == postId);
+
+  if (postIndex !== -1) {
+    // Update the post if found
+    posts[postIndex].title = title;
+    posts[postIndex].content = content;
+    res.redirect(303, '/view');
+  } else {
+    res.status(404).json({ message: 'Post not found' });
+  }
+});
+
+// Edit route
+app.get('/edit/:id', (req, res) => {
+  const postId = parseInt(req.params.id, 10);
+  
+  // Find the index of the post with the specified id
+  const postIndex = posts.findIndex(post => post.id == postId);
+
+  // If the post is found, move to edit post screen
+  if (postIndex !== -1) {
+    res.render('edit_post', { post: posts[postIndex]});
+  } else {
+    res.status(404).json({ message: 'Post not found' });
+  }
+});
+
+// Delete route
+app.get('/delete/:id', (req, res) => {
+  const postId = parseInt(req.params.id, 10);
+  
+  // Find the index of the post with the specified id
+  const postIndex = posts.findIndex(post => post.id == postId);
+
+  // If the post is found, remove it from the array
+  if (postIndex !== -1) {
+    posts.splice(postIndex, 1);
+    res.redirect(303, '/view');
+  } else {
+    res.status(404).json({ message: 'Post not found' });
+  }
+});
+
 // Post route
 app.get('/post/:id', (req, res) => {
   const postId = req.params.id;
-  console.log("postID "+ postId+ " \n posts: "+posts);
   const post = posts.find(post => post.id === postId);
 
   if (post) {
